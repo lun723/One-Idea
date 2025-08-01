ee99fc059025189437d4ef5c854967d5839066df
julia.li <julia.li@yesee.com.tw>
2025-08-01 14:41:02 +0800

    樣式調整

--- src/components/Card/Card.styles.ts
+++ src/components/Card/Card.styles.ts
@@ -1,5 +1,5 @@
 export const STYLES = {
     container: 'bg-white shadow-md rounded-lg p-6',
-    title: 'text-3xl font-bold mb-3 border-b-2 border-gray-500/50 p-4 text-center',
+    title: 'text-2xl md:text-3xl font-bold mb-3 border-b-2 border-gray-500/50 p-4 text-center',
     content: 'p-3'
 };
--- src/components/Tickers/Tickers.styles.ts
+++ src/components/Tickers/Tickers.styles.ts
@@ -1,6 +1,6 @@
 export const STYLES = {
     tickers: {
-        base: "font-bold mb-4 border-l-4 border-gray-500 px-3 py-2 bg-blue-100/30 shadow-md transform -rotate-1 transition-transform duration-200",
+        base: "font-bold mb-4 border-l-4 border-gray-500 px-3 py-2 bg-blue-100/30 shadow-md transform -rotate-1 transition-transform duration-200 break-words",
         hover: "hover:rotate-0 hover:scale-105 hover:z-10 hover:shadow-lg",
     },
 };
