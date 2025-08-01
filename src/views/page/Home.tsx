1dabfb588a17b120dd97f68c029bf70c044985a4
julia.li <julia.li@yesee.com.tw>
2025-08-01 16:33:25 +0800

    樣式調整

--- src/views/page/Home.tsx
+++ src/views/page/Home.tsx
@@ -21,7 +21,7 @@ const Home: React.FC = () => {
     <div className={globalStyles.pageContainer}>
       <div className={globalStyles.contentWrapper}>
         <div className={`${globalStyles.contentWrapper} flex items-center justify-center`}>
-          <motion.div initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8, ease: "easeOut" }} className="w-full px-2 md:px-6 py-10">
+          <motion.div initial={{ opacity: 0, x: -50 }} animate={{ opacity: 1, x: 0 }} transition={{ duration: 0.8, ease: "easeOut" }} className="w-full px-2 md:px-6 py-10 text-center">
             <h2 className="text-4xl font-bold m-6 text-gray-900">
               <TypewriterText text="Hello, I'm Juila" speed={100} isActive={true} onComplete={() => handleTextComplete("first")}/>
             </h2>
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
               <TypewriterText text="A Front-end Engineer" speed={200} isActive={textCompletion.first} onComplete={() => handleTextComplete("second")}/>
             </h2>
             <h2 className="grid md:flex text-lg sm:text-xl gap-2 md:text-2xl font-light m-8 text-gray-700 items-center justify-center">
-              <TypewriterText text="你想知道更多有關我的經歷和技能嗎？" speed={1000} isActive={textCompletion.second} onComplete={() => handleTextComplete("third")}/>
+              <TypewriterText text="想知道更多有關我的經歷和技能嗎？" speed={1000} isActive={textCompletion.second} onComplete={() => handleTextComplete("third")}/>
               <motion.div initial={{ opacity: 0 }} animate={{ opacity: textCompletion.third ? 1 : 0 }} transition={{ duration: 0.5, ease: "easeOut" }} className={globalStyles.buttonContainer}>
                 <Button onClick={() => navigate("/About")} label={<>去看看<i className="fas fa-arrow-right-long ml-2"></i></>}/>
               </motion.div>
--- src/views/style/globalStyles.ts
+++ src/views/style/globalStyles.ts
@@ -2,7 +2,6 @@ const globalStyles = {
     pageContainer: 'min-h-screen flex items-center justify-center bg-gradient-to-br from-blue-100 to-purple-100 px-2 transition-all duration-1000 ease-out',
     contentWrapper: 'w-full mt-6 max-w-5xl mx-auto px-3 md:px-8 py-16 gap-2 md:gap-12',
     gridLayout: 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-center text-center mx-auto px-8 py-8 gap-6 md:gap-12 mt-24',
-    textCenter: 'text-center',
     buttonContainer: 'flex flex-wrap gap-8 m-4 justify-center',
 };
 
