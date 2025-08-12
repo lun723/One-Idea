cff03541d9a2fa1cc98c43d883a1ba061f77e459
julia.li <julia.li@yesee.com.tw>
2025-08-12 15:26:21 +0800

    畫面新增

---
+++ src/views/page/travel/travel.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import globalStyles from "../../style/globalStyles";
+
+const Travel: React.FC = () => {
+
+    return (
+        <div className={globalStyles.pageContainer}>
+            <div className={globalStyles.contentWrapper}>
+                <div className={`${globalStyles.contentWrapper} flex items-center justify-center`}>
+                
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Travel;
